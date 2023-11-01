c8d6fd0654acec74d4d4f9afacee8a9ea6c325a4
hungtq1016 <hungtq1016@gmail.com>
2023-11-01 13:47:38 +0700

    update product controller

--- OrderTaobao/Controllers/ProductsController.cs
+++ OrderTaobao/Controllers/ProductsController.cs
@@ -9,72 +9,72 @@ namespace BaseSource.BackendAPI.Controllers
     [Authorize]
     public class ProductsController : ControllerBase
     {
-        private readonly IProductService _productService;
-        public ProductsController(IProductService productService)
+        private readonly IService<Product> _service;
+        public ProductsController(IService<Product> service)
         {
-            _productService = productService;
+            _service = service;
         }
 
         [HttpGet("page")]
         public async Task<IActionResult> GetPagedData([FromQuery] PaginationRequest request)
         {
-            var result = await _productService.GetPagedData(request, Request.Path.Value!,true);
+            var result = await _service.GetPagedData(request, Request.Path.Value!, true);
             return StatusCode(result.StatusCode, result);
         }
 
         [HttpGet("page-disable")]
         public async Task<IActionResult> GetPagedDisableData([FromQuery] PaginationRequest request)
         {
-            var result = await _productService.GetPagedData(request, Request.Path.Value!, false);
+            var result = await _service.GetPagedData(request, Request.Path.Value!, false);
             return StatusCode(result.StatusCode, result);
         }
 
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            var result = await _productService.Get();
+            var result = await _service.Get();
             return StatusCode(result.StatusCode, result);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(string id)
         {
-            var result = await _productService.GetById(id);
+            var result = await _service.GetById(id);
             return StatusCode(result.StatusCode, result);
         }
 
         [HttpPost("{user}")]
-        public async Task<IActionResult> Post(Product request,string user)
+        public async Task<IActionResult> Post(Product request, string user)
         {
-            var result = await _productService.Add(request, user);
+            var result = await _service.Add(user, request);
             return StatusCode(result.StatusCode, result);
         }
 
         [HttpPut("{id}/{user}")]
-        public async Task<IActionResult> Update(string id,string user,Product request)
+        public async Task<IActionResult> Update(string id, string user, Product request)
         {
-            var result = await _productService.Update(id,user,request);
+            var result = await _service.Update(id, user, request);
             return StatusCode(result.StatusCode, result);
         }
 
         [HttpDelete("delete/{id}/{user}")]
         public async Task<IActionResult> Enable(string id, string user)
         {
-            var result = await _productService.Enable(id, user,false);
+            var result = await _service.Enable(id, user, false);
             return StatusCode(result.StatusCode, result);
         }
 
         [HttpPut("restore/{id}/{user}")]
         public async Task<IActionResult> Restore(string id, string user)
         {
-            var result = await _productService.Enable(id, user, true);
+            var result = await _service.Enable(id, user, true);
             return StatusCode(result.StatusCode, result);
         }
 
         [HttpDelete("erase/{id}/{user}")]
         public async Task<IActionResult> Erase(string id, string user)
         {
-            var result = await _productService.Erase(id, user);
+            var result = await _service.Erase(id, user);
             return StatusCode(result.StatusCode, result);
         }
     }
