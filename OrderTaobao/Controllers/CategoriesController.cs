a736f62071e1054561a06e7b4ee32f0e74d40a12
hungtq1016 <hungtq1016@gmail.com>
2023-11-01 13:23:45 +0700

    #15 add category crud feature

---
+++ OrderTaobao/Controllers/CategoriesController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaseSource.BackendAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly IService<Category> _service;
+        public CategoriesController(IService<Category> service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("page")]
+        public async Task<IActionResult> GetPagedData([FromQuery] PaginationRequest request)
+        {
+            var result = await _service.GetPagedData(request, Request.Path.Value!, true);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet("page-disable")]
+        public async Task<IActionResult> GetPagedDisableData([FromQuery] PaginationRequest request)
+        {
+            var result = await _service.GetPagedData(request, Request.Path.Value!, false);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _service.Get();
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var result = await _service.GetById(id);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpPost("{user}")]
+        public async Task<IActionResult> Post(Category request, string user)
+        {
+            var result = await _service.Add(user,request);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpPut("{id}/{user}")]
+        public async Task<IActionResult> Update(string id, string user, Category request)
+        {
+            var result = await _service.Update(id, user, request);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpDelete("delete/{id}/{user}")]
+        public async Task<IActionResult> Enable(string id, string user)
+        {
+            var result = await _service.Enable(id, user, false);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpPut("restore/{id}/{user}")]
+        public async Task<IActionResult> Restore(string id, string user)
+        {
+            var result = await _service.Enable(id, user, true);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpDelete("erase/{id}/{user}")]
+        public async Task<IActionResult> Erase(string id, string user)
+        {
+            var result = await _service.Erase(id, user);
+            return StatusCode(result.StatusCode, result);
+        }
+    }
+}
