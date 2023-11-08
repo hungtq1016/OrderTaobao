6d38d6fa2e69e7b735950d4187cf9765567f313e
hungtq1016 <hungtq1016@gmail.com>
2023-11-08 15:32:05 +0700

    fix : clean code

--- OrderTaobao/Controllers/CategoriesController.cs
+++ OrderTaobao/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
-﻿using BaseSource.Dto.Request;
+﻿using BaseSource.BackendAPI.Authorization;
+using BaseSource.Dto.Request;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,6 +7,7 @@ namespace BaseSource.BackendAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class CategoriesController : ControllerBase
     {
         private readonly IService<Category> _service;
@@ -16,7 +18,6 @@ namespace BaseSource.BackendAPI.Controllers
 
         // GET: api/Categories/page
         [HttpGet("page")]
-        [Authorize(Policy = "AtLeast21")]
         public async Task<IActionResult> GetPagedData([FromQuery] PaginationRequest request)
         {
             var result = await _service.GetPagedData(request, Request.Path.Value!, true);
@@ -25,6 +26,7 @@ namespace BaseSource.BackendAPI.Controllers
 
         // GET: api/Categories/page-disable
         [HttpGet("page-disable")]
+        [ClaimRequirement("permission", "disable.view")]
         public async Task<IActionResult> GetPagedDisableData([FromQuery] PaginationRequest request)
         {
             var result = await _service.GetPagedData(request, Request.Path.Value!, false);
@@ -33,6 +35,7 @@ namespace BaseSource.BackendAPI.Controllers
 
         // GET: api/Categories
         [HttpGet]
+        [AllowAnonymous]
         public async Task<IActionResult> Get()
         {
             var result = await _service.Get();
@@ -41,6 +44,7 @@ namespace BaseSource.BackendAPI.Controllers
 
         // GET: api/Categories/123
         [HttpGet("{id}")]
+        [AllowAnonymous]
         public async Task<IActionResult> GetById(string id)
         {
             var result = await _service.GetById(id);
@@ -49,6 +53,7 @@ namespace BaseSource.BackendAPI.Controllers
 
         // POST: api/Categories/usersubmit
         [HttpPost("{user}")]
+        [ClaimRequirement("permission", "category.add")]
         public async Task<IActionResult> Post(Category request, string user)
         {
             var result = await _service.Add(user, request);
@@ -57,6 +62,7 @@ namespace BaseSource.BackendAPI.Controllers
 
         // PUT: api/Categories/123/usersubmit
         [HttpPut("{id}/{user}")]
+        [ClaimRequirement("permission", "category.edit")]
         public async Task<IActionResult> Update(string id, string user, Category request)
         {
             var result = await _service.Update(id, user, request);
@@ -64,16 +70,18 @@ namespace BaseSource.BackendAPI.Controllers
         }
 
         // DELETE: api/Categories/123/usersubmit
-        [HttpDelete("delete/{id}/{user}")]
-        public async Task<IActionResult> Enable(string id, string user)
+        [HttpPut("disable/{id}/{user}")]
+        [ClaimRequirement("permission", "category.edit")]
+        public async Task<IActionResult> Disable(string id, string user)
         {
             var result = await _service.Enable(id, user, false);
             return StatusCode(result.StatusCode, result);
         }
 
         // DELETE: api/Categories
-        [HttpDelete("delete/multiple")]
-        public async Task<IActionResult> MultipleEnable(MultipleRequest request)
+        [HttpPut("disable/multiple")]
+        [ClaimRequirement("permission", "category.edit")]
+        public async Task<IActionResult> MultipleDisable(MultipleRequest request)
         {
             var result = await _service.MultipleEnable(request, false);
             return StatusCode(result.StatusCode, result);
@@ -81,6 +89,7 @@ namespace BaseSource.BackendAPI.Controllers
 
         // PUT: api/Categories/restore/123/usersubmit
         [HttpPut("restore/{id}/{user}")]
+        [ClaimRequirement("permission", "category.edit")]
         public async Task<IActionResult> Restore(string id, string user)
         {
             var result = await _service.Enable(id, user, true);
@@ -88,7 +97,8 @@ namespace BaseSource.BackendAPI.Controllers
         }
 
         // PUT: api/Categories/delete/multiple
-        [HttpPut("delete/multiple")]
+        [HttpPut("restore/multiple")]
+        [ClaimRequirement("permission", "category.edit")]
         public async Task<IActionResult> MultipleRestore(MultipleRequest request)
         {
             var result = await _service.MultipleEnable(request, true);
@@ -97,6 +107,7 @@ namespace BaseSource.BackendAPI.Controllers
 
         // DELETE: api/Categories/erase/123/usersubmit
         [HttpDelete("erase/{id}")]
+        [ClaimRequirement("permission", "category.delete")]
         public async Task<IActionResult> Erase(string id)
         {
             var result = await _service.Erase(id);
@@ -105,10 +116,32 @@ namespace BaseSource.BackendAPI.Controllers
 
         // DELETE: api/Categories/erase/multiple
         [HttpDelete("erase/multiple")]
+        [ClaimRequirement("permission", "category.delete")]
         public async Task<IActionResult> MultipleErase(MultipleRequest request)
         {
             var result = await _service.MultipleErase(request);
             return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet("excel")]
+        // GET: api/Categories/excel
+        public async Task<IActionResult> Export()
+        {
+            var export = await _service.Export();
+
+            if (export is null)
+                return NotFound();
+
+            return File(export.File, export.Extension, export.Name);
+        }
+
+
+        [HttpPost("excel")]
+        // POST: api/Categories/excel
+        public async Task<IActionResult> Import(IFormFile file)
+        {
+            var result = await _service.Import(file);
+            return Ok(result);
         }
     }
 }
--- OrderTaobao/Controllers/ProductsController.cs
+++ OrderTaobao/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using BaseSource.Dto.Request;
+﻿using BaseSource.BackendAPI.Authorization;
+using BaseSource.Dto.Request;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -7,6 +8,7 @@ namespace BaseSource.BackendAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ProductsController : ControllerBase
     {
         private readonly IService<Product> _service;
@@ -25,6 +27,7 @@ namespace BaseSource.BackendAPI.Controllers
 
         // GET: api/Products/page-disable
         [HttpGet("page-disable")]
+        [ClaimRequirement("permission", "disable.view")]
         public async Task<IActionResult> GetPagedDisableData([FromQuery] PaginationRequest request)
         {
             var result = await _service.GetPagedData(request, Request.Path.Value!, false);
@@ -33,6 +36,7 @@ namespace BaseSource.BackendAPI.Controllers
 
         // GET: api/Products
         [HttpGet]
+        [AllowAnonymous]
         public async Task<IActionResult> Get()
         {
             var result = await _service.Get();
@@ -49,6 +53,7 @@ namespace BaseSource.BackendAPI.Controllers
 
         // POST: api/Products/usersubmit
         [HttpPost("{user}")]
+        [ClaimRequirement("permission", "product.add")]
         public async Task<IActionResult> Post(Product request, string user)
         {
             var result = await _service.Add(user, request);
@@ -57,6 +62,7 @@ namespace BaseSource.BackendAPI.Controllers
 
         // PUT: api/Products/123/usersubmit
         [HttpPut("{id}/{user}")]
+        [ClaimRequirement("permission", "product.edit")]
         public async Task<IActionResult> Update(string id, string user, Product request)
         {
             var result = await _service.Update(id, user, request);
@@ -64,16 +70,18 @@ namespace BaseSource.BackendAPI.Controllers
         }
 
         // DELETE: api/Products/123/usersubmit
-        [HttpDelete("delete/{id}/{user}")]
-        public async Task<IActionResult> Enable(string id, string user)
+        [HttpPut("disable/{id}/{user}")]
+        [ClaimRequirement("permission", "product.edit")]
+        public async Task<IActionResult> Disable(string id, string user)
         {
             var result = await _service.Enable(id, user, false);
             return StatusCode(result.StatusCode, result);
         }
 
         // DELETE: api/Products
-        [HttpDelete("delete/multiple")]
-        public async Task<IActionResult> MultipleEnable(MultipleRequest request)
+        [HttpPut("disable/multiple")]
+        [ClaimRequirement("permission", "product.edit")]
+        public async Task<IActionResult> MultipleDisable(MultipleRequest request)
         {
             var result = await _service.MultipleEnable(request, false);
             return StatusCode(result.StatusCode, result);
@@ -81,6 +89,7 @@ namespace BaseSource.BackendAPI.Controllers
 
         // PUT: api/Products/restore/123/usersubmit
         [HttpPut("restore/{id}/{user}")]
+        [ClaimRequirement("permission", "product.edit")]
         public async Task<IActionResult> Restore(string id, string user)
         {
             var result = await _service.Enable(id, user, true);
@@ -89,6 +98,7 @@ namespace BaseSource.BackendAPI.Controllers
 
         // PUT: api/Products/delete/multiple
         [HttpPut("delete/multiple")]
+        [ClaimRequirement("permission", "product.edit")]
         public async Task<IActionResult> MultipleRestore(MultipleRequest request)
         {
             var result = await _service.MultipleEnable(request, true);
@@ -97,6 +107,7 @@ namespace BaseSource.BackendAPI.Controllers
 
         // DELETE: api/Products/erase/123/usersubmit
         [HttpDelete("erase/{id}")]
+        [ClaimRequirement("permission", "product.delete")]
         public async Task<IActionResult> Erase(string id)
         {
             var result = await _service.Erase(id);
@@ -105,10 +116,33 @@ namespace BaseSource.BackendAPI.Controllers
 
         // DELETE: api/Products/erase/multiple
         [HttpDelete("erase/multiple")]
+        [ClaimRequirement("permission", "product.delete")]
         public async Task<IActionResult> MultipleErase(MultipleRequest request)
         {
             var result = await _service.MultipleErase(request);
             return StatusCode(result.StatusCode, result);
+        }
+
+
+        [HttpGet("excel")]
+        // GET: api/Products/excel
+        public async Task<IActionResult> Export()
+        {
+           var export = await _service.Export();
+
+           if (export is null)
+                return NotFound();
+
+            return File(export.File,export.Extension, export.Name);
+        }
+
+
+        [HttpPost("excel")]
+        // POST: api/Products/excel
+        public async Task<IActionResult> Import(IFormFile file)
+        {
+            var result = await _service.Import(file);
+            return Ok(result);
         }
     }
 }
