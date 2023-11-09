e5f2dcabe67a780beaa5784358c5404be19d4db6
hungtq1016 <hungtq1016@gmail.com>
2023-11-09 23:11:22 +0700

    feature : global handler 500 error

---
+++ OrderTaobao/Extensions/Middleware.cs
@@ -0,0 +1,12 @@
+﻿using BaseSource.BackendAPI.Middlewares;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class Middleware
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
