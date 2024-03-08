050227f1b1f9b618c5d3e5a042e9b165695c3f6f
hungtq1016 <hungtq1016@gmail.com>
2024-03-08 17:39:07 +0700

    enhancement: email features

---
+++ src/services/MailService/Models/OTP.cs
@@ -0,0 +1,9 @@
+﻿namespace MailService.Models
+{
+    public class OTP : Entity
+    {
+        public string Email { get; set; }
+        public DateTime ExpiredTime { get; set; }
+        public int Code { get; set; }
+    }
+}
--- src/services/MailService/Program.cs
+++ src/services/MailService/Program.cs
@@ -1,4 +1,13 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using MailService.Infrastructure;
+using Newtonsoft.Json;
+using MailService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 
@@ -6,6 +15,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IRabbitMQService, RabbitMQService>();
 
 var app = builder.Build();
 
@@ -15,6 +25,34 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+var rabbitMQService = new RabbitMQService();
+
+// Tạo một instance của EmailService để xử lý thông điệp nhận được từ RabbitMQ
+var emailService = new BaseMailServices();
+
+// Đăng ký EmailService như một IMessageHandler
+rabbitMQService.ConsumeMessage(message =>
+{
+    var resetPasswordRequest = JsonConvert.DeserializeObject<ResetPasswordRequest>(message);
+
+    // Xử lý thông điệp
+    Console.WriteLine("Received ResetPasswordRequest");
+    Console.WriteLine("UserId: {0}, Token: {1}", resetPasswordRequest.Email, resetPasswordRequest.Password);
+
+    emailService.SendEmail(resetPasswordRequest.Email);
+}, "ResetPasswordRequest");
+
+rabbitMQService.ConsumeMessage(message =>
+{
+    var otp = JsonConvert.DeserializeObject<OTP>(message);
+
+    // Xử lý thông điệp
+    Console.WriteLine("Received ResetPasswordRequest");
+    Console.WriteLine("UserId: {0}, Token: {1}", otp.Email, otp.Code);
+
+    emailService.SendEmailOTP(otp);
+}, "OTP");
 
 app.UseHttpsRedirection();
 
